33339ac3946a72844452160d46b5c347f25042c5
sallllbilal/ <salmakeevbili2008@gmail.com>
2023-06-26 21:31:43 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,117 @@
+// let, const, var
+// 1)Должно содержать только буквы и цифры
+// 2)Первый символ не должен начинаться с цифры
+
+// let name +
+// let name1 +
+// let $name +
+// let _name +
+// let 2132name +
+// let имя (не желательно)
+// let Name (не желательно)
+
+let num = 13;
+num = 23;
+// let num = 23 нельзя создавать переменную с существующим названием
+
+// const num2 = 23;
+// num2 = 24
+// console.log (name)
+// var name = "Bilal"
+
+let name = "Bilal";
+// console.log (name)
+name = "Daud";
+// console.log (name)
+
+// const brand = "Adidas"
+// brand = Nike
+// console.log (brand)
+
+// ----------  Типы данных --------//
+
+// let id;
+// console.log(typeof id) // undefined
+
+// id = 45;
+// console.log(typeof id) // number
+
+// id = 45n
+// console.log(typeof id) // bigint
+
+// id = "45"
+// console.log (typeof id); // string
+
+// id = null
+// console.log (typeof null); // object
+
+// id = {
+//     name: "bilal",
+//     age: 15
+// }
+
+// console.log (typeof id); //object
+
+// id = false;
+// console.log (typeof id) // boolean
+
+// id = Symbol("bilal");
+// console.log (typeof id); //symbol
+
+// const person = {
+//     name: "john",
+//     age: 28,
+//     job: "Frontend developer",
+//     car: {
+//         model: "BMW",
+//         color: "Green",
+//     }
+// }
+
+// console.log(person.name)
+// console.log(person.car.model)
+// const colorOfCar = peraon.car.color;
+// console.log(colorOfCar)
+// --------------Массивы ------------------
+
+// const mentors = ["Aijamal", "Abbaz", "Mirbek"];
+
+// console.log(mentors[0]);
+
+// let randomArray = ["hello", null, 25, true, {model: 'BMW'}, undefined]
+// const objectFromArray = randomArray [4]
+
+// console.log(randomArray.length)
+// console.log(randomArray[randomArray.length - 1])
+
+// --------- Арифметические операции --------------
+console.log(1 + 1);
+console.log(1 - 1);
+console.log(1 * 1);
+console.log(1 / 1);
+
+const num1 = 30;
+const num2 = 40;
+
+const results = num1 * num2;
+console.log(result2);
+
+const result2 = result - 20;
+console.log(result2);
+
+console.log(2 ** 4);
+
+const a = 10 % 7;
+console.log(a);
+
+const b = "Bilal" + 5;
+console.log(b);
+
+ы;
+
+// console.log (5 !=5)
+
+console.log(5 == "5");
+console.log(5 === "5");
+
+иио;
